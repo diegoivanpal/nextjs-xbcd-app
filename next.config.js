5312c04e5c2bf18092f69a65cd3ae086e703604b
diegoivanpal <58339715+diegoivanpal@users.noreply.github.com>
2022-08-25 10:05:22 -0300

    a

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['imgs.xkcd.com']
+  },
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en'
+  }
 }
 
 module.exports = nextConfig
